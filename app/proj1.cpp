b109de9dd5571d13ad08c7ab8b78cb9dccc0c160
Michael Shindler <mikes@uci.edu>
2022-09-27 15:12:35 -0700

    Initial Commit

---
+++ app/proj1.cpp
@@ -0,0 +1,11 @@
+#include "proj1.hpp"
+#include <unordered_map>
+#include <string>
+
+
+bool puzzleSolver(std::string s1, std::string s2, std::string s3, std::unordered_map<char, unsigned> & mapping)
+{
+	return true; // This is not the correct general solution. 
+}
+
+
